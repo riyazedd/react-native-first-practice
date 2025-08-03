1a6a27d6ab6208bebcb9c74986e6d08bf7276174
riyazedd <riyaz.shrestha00@gmail.com>
2025-08-03 19:32:03 +0545

    React Native Practice Started

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, Image, Button, TouchableOpacity, Pressable, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const onButtonPress=()=>Alert.alert("Button Pressed!")
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Text style={styles.title}>Hello World!</Text>
+
+				<Text style={styles.subTitleText}>
+					Lorem ipsum dolor sit, amet
+					<Text style={styles.clickText} onPress={() => Alert.alert("Text Pressed!")}>
+						{" "}
+						Press Here!{" "}
+					</Text>
+					consectetur adipisicing elit. Illum facilis voluptates commodi ratione
+					nobis quis voluptatem non, qui fuga nesciunt esse enim laudantium
+					eligendi accusamus modi nam expedita quae architecto.
+				</Text>
+
+				<View style={styles.imgContainer}>
+					<Image source={require("./assets/icon.png")} style={styles.image1} />
+
+					<Image source={require("./assets/icon.png")} style={styles.image1} />
+
+					<TouchableOpacity onLongPress={onButtonPress}>
+						<Image source={{uri:"https://picsum.photos/200"}} style={styles.image2} blurRadius={10} />
+					</TouchableOpacity>
+
+					<Button title="Press Me!" onPress={onButtonPress}  />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		padding: 20,
+	},
+
+	title: {
+		textAlign: "center",
+		fontSize: 40,
+		fontWeight: "bold",
+	},
+
+	subTitleText: {
+		textAlign: "justify",
+	},
+
+	clickText: {
+		color: "red",
+		textDecorationLine: "underline",
+	},
+
+	imgContainer:{
+		alignItems:"center",
+		marginTop:10
+	},
+
+	image1:{
+		width:300,
+		height:300,
+	},
+	image2:{
+		width:300,
+		height:300,
+		marginTop:10,
+		borderRadius:150
+	},
+	button:{
+		marginTop:10,
+
+	}
 });
